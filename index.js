3a1335821c42f7dd843d7885f68bd2872a85d493
Dart2.0 <59048496+Dart2004@users.noreply.github.com>
2020-12-09 11:29:09 +0100

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ app.get('/', function(req, res){
 });
 
 io.on('connection', function(socket){
+  io.emit('chat message', "New User joined the chat");
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
   });
